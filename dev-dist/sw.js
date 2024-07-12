051d036b512fae86c6e1973961ed75005cc8b756
Mahmoud Abdalrahman Mohamed <mahnoudhouda5632@gmail.com>
2024-07-12 21:13:22 +0300

    add

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6e3354dc'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8p4c3uoe638"
+    "revision": "0.5agi2t9m3fo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
